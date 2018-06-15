0866bd75df5f775761d2a284601017beac76fd1f
kamil.sarelo@dynatrace.com <kamil.sarelo@dynatrace.com>
2018-06-15 16:48:56 +0200

    update test

--- derStandard.at.user.js
+++ derStandard.at.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       derStandard.at
-// @version    1.0
+// @version    2
 // @namespace  https://github.com/kamilsarelo
 // @author     kamilsarelo
 // @update     https://github.com/kamilsarelo/violentmonkey/raw/master/derStandard.at.user.js
