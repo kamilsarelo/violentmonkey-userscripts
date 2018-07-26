976d488f30ae3f6f8a9a41cc90c60ce30b84bfbd
kamilsarelo <kamil.sarelo@gmail.com>
2018-07-26 13:02:17 +0200

    version

--- derStandard.at.user.js
+++ derStandard.at.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name       derStandard.at
-// @version    2.0.0
+// @version    2.0.1
 // @namespace  https://github.com/kamilsarelo
 // @author     kamilsarelo
 // @update     https://github.com/kamilsarelo/violentmonkey/raw/master/derStandard.at.user.js
