9406ede3b11c7c1ae8e841b0e2a89b1b23ec31ac
kamilsarelo <kamil.sarelo@gmail.com>
2019-02-25 11:59:19 +0100

    initial commit

---
+++ dynatrace.org.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name       dynatrace.org
+// @version    1
+// @namespace  https://github.com/kamilsarelo
+// @author     kamilsarelo
+// @update     https://github.com/kamilsarelo/violentmonkey/raw/master/dynatrace.org.user.js
+// @icon       https://raw.githubusercontent.com/kamilsarelo/violentmonkey/master/dynatrace.org.logo.png
+// @grant      none
+// @include    *://devops-rx.lab.dynatrace.org/*
+// @require    https://cdnjs.cloudflare.com/ajax/libs/mark.js/8.11.1/mark.min.js
+// ==/UserScript==
